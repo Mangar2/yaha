a28896ea07af01d20df4faeb0e5aa6539e6c10c6
Mangar <Mangar@I3T1>
2019-10-26 10:51:29 +0200

    introduced version

---
+++ bridge/bridge.js
@@ -0,0 +1,80 @@
+/**
+ * ---------------------------------------------------------------------------------------------------
+ * This software is licensed under the GNU LESSER GENERAL PUBLIC LICENSE Version 3. It is furnished
+ * "as is", without any support, and with no warranty, express or implied, as to its usefulness for
+ * any purpose.
+ *
+ * File:      broker.js
+ *
+ * Author:      Volker Böhm
+ * Copyright:   Volker Böhm
+ * Version:     1.0
+ * ---------------------------------------------------------------------------------------------------
+ */
+
+'use strict';
+const Receive = require("@mangar2/receive")
+const Publish = require("@mangar2/publish");
+const UnitTest = require("@mangar2/unittest");
+const Message = require("@mangar2/message");
+const Connect = require("@mangar2/connect");
+const shutdown = require("@mangar2/shutdown");
+const Broker = require("@mangar2/broker");
+const TopicStore = require("@mangar2/topicstore");
+
+const HOST = "192.168.0.4";
+const PORT = 8183;
+const MY_PORT = 9002;
+
+let connect = new Connect("/broker/client", HOST, PORT, MY_PORT);
+let publish = new Publish(HOST, PORT, { retry: 10});
+let receive = new Receive(MY_PORT);
+let store = new TopicStore();
+let amount = 0;
+
+receive.on("publish", (message, qos, dup) => {
+    console.log("%s (qos%s), value:%s, dup:%s", message.topic, qos, message.value, dup);
+    store.addData(message.topic, message.value, message.reason);
+    amount++;
+    if (amount % 20 == 0) {
+        store.persist("./", "bridge")
+    }
+});
+
+async function subscribe_v0(topics) {
+    let payload = {
+        clientId: connect.clientId,
+        subscribe: {QoS:0, topics:["#"]}
+    }
+    let packetid = connect.providePacketId();
+    let result = await connect.putJsonMessage("/subscribe", {packetid}, payload);
+    let headers = result.headers;
+
+    if (result.statusCode !== 200) {
+        throw(Error("Unable to subscribe status code: " + statusCode + " headers " + JSON.stringify(headers)));
+    }
+
+    return JSON.parse(result.payload);
+}
+
+async function connectToBroker(clean) {
+    let topics = {
+        '/#' : 0
+    }
+    try {
+        await connect.connect(clean);
+    }
+    catch (err) {
+        console.error(err);
+    }
+    connect.isConnected = true;
+    try {
+        await subscribe_v0(topics);
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
+
+connectToBroker(true);
+receive.listen();

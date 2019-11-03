175fd862c99fd41d3a1a3de28ffc036c4f47c599
Mangar <Mangar@I3T1>
2019-11-03 21:29:37 +0100

    restructured connections

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,19 @@
 module.exports = {
-    env: {
-      browser: true,
-      commonjs: true,
-      es6: true
-    },
-    extends: [
-      'standard'
-    ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly'
-    },
-    parserOptions: {
-      ecmaVersion: 2018
-    },
-    rules: {
-        indent: ['warn', 4]
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
   }
+}

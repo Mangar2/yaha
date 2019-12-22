6293e296e6abdcdd37852ded0d768fd81485a89d
Mangar <Mangar@I3T1>
2019-12-22 10:04:40 +0100

    added device-details

--- control/controlapp/src/app/app-routing.module.ts
+++ control/controlapp/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DeviceDetailsComponent } from './device-details/device-details.component';
+import { DeviceListComponent } from './device-list/device-detail.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    { path: 'devicedetails/:deviceIndex', component: DeviceDetailsComponent },
+    { path: 'devicelist', component: DeviceListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- control/controlapp/src/app/app.module.ts
+++ control/controlapp/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HttpClientModule } from '@angular/common/http';
-
 import { LocationPipe } from './app.pipe';
+
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { DeviceDetailsComponent } from './device-details/device-details.component';
+import { DeviceListComponent } from './device-list/device-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TopBarComponent,
+    DeviceDetailsComponent,
+    DeviceListComponent,
     LocationPipe
   ],
   imports: [
